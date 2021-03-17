af1cbaff4eeafd37b6e5f83374b890914d81a79a
Santiago Barrera <i.medinamobiera.com>
2021-03-16 19:51:40 -0500

    async aditional test

--- __test__/async.test.js
+++ __test__/async.test.js
@@ -7,8 +7,20 @@ describe('Probar async y await', () => {
         await getDataFromApi(api).then(data => {
             expect(data.results.length).toBeGreaterThan(0);
         })
-        await getDataFromApi(getRick).then(data=>{
+        await getDataFromApi(getRick).then(data => {
             expect(data.name).toEqual('Rick Sanchez')
         })
     })
+
+    test('Realizar peticion APi con error', async () => {
+        const api = 'http://httpstat.us/404'
+        const peticion = getDataFromApi(api);
+        await expect(peticion).rejects.toEqual(Error('Request failed with status code 404'))
+    })
+    test('RResuelve hola', async () => {
+        await expect(Promise.resolve('hola')).resolves.toBe('hola')
+        await expect(Promise.reject('err')).rejects.toBe('err')
+    })
+
+
 })
